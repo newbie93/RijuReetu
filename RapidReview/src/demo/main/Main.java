bb60c30ff45b85838dbbfe71364b8947a731a6f7
newbie93 <rizus2@gmail.com>
2018-09-16 00:36:44 +0530

    bubble sort

--- RapidReview/src/demo/main/Main.java
+++ RapidReview/src/demo/main/Main.java
@@ -1,9 +1,14 @@
 package demo.main;
 
+import demo.sort.bubble.BubbleSort;
+
 public class Main {
 	
 	public static void main(String[]args) {
-		
+		int array[]={-1,5,4,3,2,1},newArray[];
+		newArray=BubbleSort.sort(array);
+		for(int i:newArray)
+			System.out.println(i);
 	}
 
 }
